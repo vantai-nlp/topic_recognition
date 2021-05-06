92aa06972a49d2c264113e7695cdf0c11eac680c
vantai-050101 <vantai.050101@gmail.com>
2021-05-06 14:36:09 +0700

    init files

---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import os
+
+
+# load data from file txt
+def load_data(name_file, max_size = 10):
+        category = name_file.split('/')[-2].split('.')[0]
+        file = open(name_file, 'r')
+        X = file.readlines()
+
+        if len(X) > max_size:
+            X = X[:max_size]
+
+        y = [category for i in range(len(category))]
+        return X, y
+
+
+def text2words(text):
+    words = text.split()[:-1]
+    return words
+
+
+
+
+
+
+X, y = load_data('./dataset/English/Computer/evc.train.en')
+text = 'hi everyone , my name is Van Tai .\n'
+print(text2words(text))
